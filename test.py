5ab7539ed6cecafdfb9dd32ef5cd2cb998a77504
Faust VIII <joh.faustviii@gmail.com>
2022-06-29 22:10:56 +0200

    Refactor code of PoC

--- test.py
+++ test.py
@@ -1,44 +1,48 @@
+from requests import Response
+from typing import Tuple
+
 import requests
 
-if __name__ == "__main__":
+def get_content(index : int) -> Response:
+    url = f"https://www.kurnik.pl/p/?g=sc{str(index)}.txt"
+    return requests.get(url = url)
 
+def is_valid_response(response : Response) -> bool:
+    return response.ok and response.content.decode()
+
+def find_valid_range() -> Tuple[int, int]:
     previous_index = 70000000
     index = 70000000
 
     while True:
-        print('index: ' + str(index))
-        url = 'https://www.kurnik.pl/p/?g=sc' + str(index) +'.txt'
-        r = requests.get(url = url)
-        if (not r.ok or len(r.content.decode()) == 0):
+        response = get_content(index)
+        if (is_valid_response(response)):
+            break
+        else:
             previous_index = index
             index += 10000
-        else:
-            break
+    
+    return previous_index, index
 
-    bottom_index = previous_index
-    up_index = index
 
-    while bottom_index != up_index:
+def find_first_valid(bottom_index : int, up_index : int) -> int:
+    while bottom_index < up_index:
         index = int((bottom_index + up_index)/2)
-        print('index: ' + str(index))
-        url = 'https://www.kurnik.pl/p/?g=sc' + str(index) +'.txt'
-        r = requests.get(url = url)
-        if (not r.ok or len(r.content.decode()) == 0):
-            bottom_index = index + 1
-        else:
+        response = get_content(index)
+        if (is_valid_response(response)): 
             up_index = index
+        else:
+            bottom_index = index + 1
+    return bottom_index
 
+if __name__ == "__main__":
 
+    bottom_index, up_index = find_valid_range()
+    index = find_first_valid(bottom_index, up_index)
+    
+    response_string = get_content(index).content.decode()
 
+    print(response_string.split('\n')[6])
 
-    URL = 'https://www.kurnik.pl/p/?g=sc' + str(index) + '.txt'
-
-    rs = requests.get(url = URL)
-    r = rs.content.decode()
-
-    print(r.split('\n')[6])
-
-    for s in r.split('\n')[12:]:
+    for s in response_string.split('\n')[12:]:
         print(s)
-
-    print(len(r))
