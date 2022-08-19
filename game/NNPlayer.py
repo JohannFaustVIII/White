62792b904a96438c683b2558ae2a824e339473e0
Faust VIII <joh.faustviii@gmail.com>
2022-08-19 23:33:26 +0200

    Add idea of deciding about a move by NNPlayer

--- game/NNPlayer.py
+++ game/NNPlayer.py
@@ -7,9 +7,21 @@ class NNPlayer(Player):
         super().__init__()
         # player's parameters have to be set from the outside
         # 1. here to provide NN and check its parameters? or load here? first option looks better
-        # 2. add setting flag to register moves (training to be done outside) <- that requires a separate class for move win/lose count
+        # 2. add setting flag to register moves (training to be done outside) <- that requires a list of end positions of this player
         # 3. pass discover parameter to enable discovering new moves
 
     def get_move(self, map : Map) -> int:
-        #TODO : to implement, more like to be implemented by extending classes
+        # if random less than discover:
+        #   get available moves
+        #   choose random of them
+        #   save state after move ??? <- is it required? if move will be returned outside, then state after move can be saved there?
+        #   return move
+        # else:
+        #   get available move
+        #   for each available move:
+        #       get state of map after move
+        #       get state prediction from NN (value between 0 to 1, 0 - state lost, 1 - state win)
+        #       decide about maximum <- the closest to win
+        #   save state after move ??? <- again to think
+        #   return move making a state the closest to a win
         pass
