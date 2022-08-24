bf907f56d3cf50c7d5c1879e8c6f56857c627fd9
Faust VIII <joh.faustviii@gmail.com>
2022-08-24 22:45:06 +0200

    Add handling NN model by NNPlayer

--- game/NNPlayer.py
+++ game/NNPlayer.py
@@ -1,23 +1,23 @@
 import random
+
+from tensorflow.keras.models import Sequential
+
 from Map import Map
 from Player import Player
 
 class NNPlayer(Player):
 
-    def __init__(self, discover : float) -> None:
+    def __init__(self, model: Sequential, discover : float) -> None:
         super().__init__()
-        # player's parameters have to be set from the outside
-        # 1. here to provide NN and check its parameters? or load here? first option looks better
+        self.model = model
         self.discover = discover
 
     def get_move(self, map : Map, first_player : bool) -> int:
         moves = map.get_possible_moves()
         print(moves)
         if random.random() < self.discover:
-            print("Random!")
             return random.choice(moves)
         else:
-            print("Value!")
-            valued_moves = [(move, random.random(), map.get_points_for_move(move, first_player)) for move in moves] # the state pass to NN, get state prediction from NN (value between 0 to 1, 0 - state lost, 1 - state win)
+            valued_moves = [(move, self.model.preditct(map.get_points_for_move(move, first_player))) for move in moves]
             max_move = max(valued_moves, key=lambda x: x[1])
             return max_move[0]
