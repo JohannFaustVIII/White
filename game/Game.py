087b5a21dc41891eaae873afd057dbaeed7bc16e
Faust VIII <joh.faustviii@gmail.com>
2022-08-25 21:59:13 +0200

    Add registering moves in Game class

--- game/Game.py
+++ game/Game.py
@@ -7,18 +7,25 @@ class Game:
     is_first_player_win = False
     is_second_player_win = False
 
-    def __init__(self, first_player : Player, second_player : Player) -> None:
+    def __init__(self, first_player : Player, second_player : Player, register_moves : bool = False) -> None:
         self.__map = Map(8, 10)
         self.__first_player = first_player
         self.__second_player = second_player
+        
+        self.__register_moves = register_moves
+        self.__first_player_states = []
+        self.__second_player_states = []
     
     def play_game(self):
         is_first_player_move = True
         while self.__map.is_end_of_game():
             if not self.__map.is_continuous_move_possible():
                 is_first_player_move = not is_first_player_move
-            move = self.__first_player(self.__map) if is_first_player_move else self.__second_player(self.__map)
+            move = self.__first_player.get_move(self.__map) if is_first_player_move else self.__second_player.get_move(self.__map)
             self.__map.make_move(move, is_first_player_move)
+            if self.__register_moves:
+                state = self.__map.get_points(is_first_player_move)
+                self.__first_player_states.append(state) if is_first_player_move else self.__second_player_states.append(state)
         self.is_game_finished = True
         self.decide_winner(is_first_player_move)
 
