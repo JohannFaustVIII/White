a6b197a23c48bfc8cba65ebc1533822085d6d7e0
Faust VIII <joh.faustviii@gmail.com>
2022-08-26 23:13:42 +0200

    Add idea for StateData class

---
+++ game/StateData.py
@@ -0,0 +1,12 @@
+class StateData:
+
+    __loses = 0
+    __wins = 0
+    __total = 0
+
+    def __init__(self):
+        # TODO: should a state be passed here? StateData will be in a dict(?) so no need to keep it here?
+        # the question is if a list can be a key, or it would be mapped to a str(?) and a list should be kept here for train purposes
+        # to think
+        pass
+    
