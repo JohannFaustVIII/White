2b9dd874a7daeb2fb767fc910ff36df75f89cf6b
Faust VIII <joh.faustviii@gmail.com>
2022-08-29 22:57:49 +0200

    Add returning stats by StateData

--- game/StateData.py
+++ game/StateData.py
@@ -1,12 +1,27 @@
+from functools import reduce
+
+
 class StateData:
 
     __loses = 0
     __wins = 0
-    __total = 0
 
-    def __init__(self):
-        # TODO: should a state be passed here? StateData will be in a dict(?) so no need to keep it here?
-        # the question is if a list can be a key, or it would be mapped to a str(?) and a list should be kept here for train purposes
-        # to think
+    def __init__(self, state : list[int]):
+        self.__state = state
         pass
     
+    def increase_loses(self) -> None:
+        self.__loses = self.__loses + 1
+    
+    def increase_wins(self) -> None:
+        self.__wins = self.__wins + 1
+
+    def get_win_percentage(self) -> float:
+        sum = self.__loses + self.__wins
+        return self.__wins/sum if sum != 0 else 0
+
+    def get_state(self) -> list[int]:
+        return self.__state
+
+    def state_to_string(state : list[int]) -> str:
+        return reduce(lambda x, y: str(x) + str(y), state)
