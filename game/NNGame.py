6339306ce0e446c1db5dd3072e024560f3264892
Faust VIII <joh.faustviii@gmail.com>
2022-08-30 20:42:07 +0200

    Refactor updating states in NNGame

--- game/NNGame.py
+++ game/NNGame.py
@@ -13,23 +13,19 @@ class NNGame:
         for i in range(0, 100): #TODO: change number of iterations
             game = Game(player, player, True)
             game.play_game()
-            for state in game.get_winner_states(): #TODO: to refactor? looks like can be moved to a separate method, and the increase part can be refactored too
-                state_name = StateData.state_to_string(state)
-                state_to_update = self.__states_data[state_name]
-                if not state_to_update:
-                    state_to_update = StateData(state)
-                    self.__states_data[state_name] = state_to_update
-                state_to_update.increase_wins()
-                    
-            for state in game.get_loser_states():
-                state_name = StateData.state_to_string(state)
-                state_to_update = self.__states_data[state_name]
-                if not state_to_update:
-                    state_to_update = StateData(state)
-                    self.__states_data[state_name] = state_to_update
-                state_to_update.increase_loses()
+            self.__update_states(StateData.increase_wins, game.get_winner_states())
+            self.__update_states(StateData.increase_loses, game.get_loser_states())
 
         # 4. train NN with the statistics
         # 5. save NN after training
         # 6. save stats? and maybe load them before? at some point, stats should be dropped, as more experienced NN would be, to think
         # 7. go back to point 2? (it needs to be a loop too to stop at given point, or another thread, and send a signal to stop, gracefully, to think)
+
+    def __update_states(self, increase : function, states : list[list[int]]) -> None:
+        for state in states:
+            state_name = StateData.state_to_string(state)
+            state_to_update = self.__states_data[state_name]
+            if not state_to_update:
+                state_to_update = StateData(state)
+                self.__states_data[state_name] = state_to_update
+            increase(self=state_to_update)
