b40a4c75ce0ffffda03e956eb2f1603eddd64961
Faust VIII <joh.faustviii@gmail.com>
2023-03-01 15:05:44 +0100

    Pass and print name for ConsolePlayer

--- game/ConsolePlayer.py
+++ game/ConsolePlayer.py
@@ -3,9 +3,14 @@ from Player import Player
 
 class ConsolePlayer(Player):
     
+    def __init__(self, name : str = "") -> None:
+        super().__init__()
+        self.name = name
+
     def get_move(self, map : Map, first_player : bool) -> int:
         move = -1
         possible_moves = map.get_possible_moves(first_player)
+        print(self.name)
         while move not in possible_moves:
             move = int(input(f"Possible moves: {possible_moves}\nPlease, enter a move: "))
             if move not in possible_moves:
