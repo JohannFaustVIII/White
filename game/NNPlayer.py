be27f0b950154ef3c32b0d3654c11ee426ee009d
Faust VIII <joh.faustviii@gmail.com>
2023-03-01 15:05:26 +0100

    Print predicted value for each move for NNPlayer

--- game/NNPlayer.py
+++ game/NNPlayer.py
@@ -6,18 +6,24 @@ from Player import Player
 
 class NNPlayer(Player):
 
-    def __init__(self, model, discover : float, verbose : bool = False) -> None:
+    def __init__(self, model, discover : float, verbose : bool = False, extra_verbose : bool = False) -> None:
         super().__init__()
         self.model = model
         self.discover = discover
         self.verbose = verbose
+        self.extra_verbose = extra_verbose
 
     def get_move(self, map : Map, first_player : bool) -> int:
         moves = map.get_possible_moves(first_player)
         if random.random() < self.discover:
             move = random.choice(moves)
         else:
-            valued_moves = [(move, self.model.predict([map.get_points_for_move(move, first_player)], verbose = 0)) for move in moves]
+            def predict_func(move):
+                value = self.model.predict([map.get_points_for_move(move, first_player)], verbose = 0)
+                if self.extra_verbose:
+                    print(f"Move {move} value = {value}")
+                return (move, value) 
+            valued_moves = [predict_func(move) for move in moves]
             max_move = max(valued_moves, key=lambda x: x[1])
             move = max_move[0]
         if self.verbose:
