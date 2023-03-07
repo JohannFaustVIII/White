3ed95a4dd160cf031dca5d85b668b069c818353e
Faust VIII <joh.faustviii@gmail.com>
2023-03-07 14:20:18 +0100

    Script to play against AI

---
+++ game/play_vs_ai.py
@@ -0,0 +1,22 @@
+import sys
+import tensorflow as tf
+
+from ConsolePlayer import ConsolePlayer 
+from NNPlayer import NNPlayer
+from Game import Game
+
+is_first_player = sys.argv[1] == "1"
+model_file = sys.argv[2]
+
+
+model = tf.keras.models.load_model(model_file)
+model.summary()
+
+first_player = ConsolePlayer(name = "First player") if is_first_player else NNPlayer(model, 0.0, verbose = True)
+second_player = ConsolePlayer(name = "Second player") if not is_first_player else NNPlayer(model, 0.0, verbose = True)
+
+game = Game(first_player, second_player, False)
+game.play_game()
+
+print(f"First player win = {game.is_first_player_win}")
+print(f"Second player win = {game.is_second_player_win}")
