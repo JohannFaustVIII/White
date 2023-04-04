dc0cad173bab00f1ff5fccb82101307161f8ffbc
Faust VIII <joh.faustviii@gmail.com>
2023-04-04 14:04:08 +0200

    Add making moves

--- game/conv/Map.py
+++ game/conv/Map.py
@@ -2,6 +2,8 @@ from Point import Point
 
 class Map:
     
+    __second_player_moves = [4, 5, 6, 7, 0, 1, 2, 3]
+
     def __init__(self, width : int, height : int) -> None:
         self.__width = width
         self.__height = height
@@ -11,6 +13,37 @@ class Map:
 
     def get_points(self):
         return self.__points
+
+    def make_move(self, move : int, first_player : bool = True) -> None:
+        if not first_player:
+            move = Map.__second_player_moves[move]
+        self.__update_point(move, True)
+        self.__make_move(move)
+        self.__update_point((move + 4) % 8, True)
+
+    def __update_point(self, move : int, value : bool) -> None:
+        index = self.__get_current_point_index()
+        point = self.__points[index]
+        point.do_move(move, value)
+
+    def __get_current_point_index(self) -> int:
+        return (self.__position_y + 1) * (self.__width + 1) + self.__position_x
+
+    def __make_move(self, move : int) -> None:
+        left_moves = [1, 2, 3]
+        right_moves = [5, 6, 7]
+        up_moves = [7, 0, 1]
+        down_moves = [3, 4, 5]
+
+        if move in left_moves:
+            self.__position_x += 1
+        elif move in right_moves:
+            self.__position_x -= 1
+
+        if move in up_moves:
+            self.__position_y -= 1
+        elif move in down_moves:
+            self.__position_y += 1
 
     def __generate_points(width : int, height : int) -> list[Point]:
         points = []
