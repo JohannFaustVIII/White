e31572e1a7f85232a4b1c60396adea5cdcaeb6b1
Faust VIII <joh.faustviii@gmail.com>
2023-04-18 13:20:44 +0200

    Play vs conv model

---
+++ game/conv/ConsolePlayer.py
@@ -0,0 +1,18 @@
+from Map import Map
+from Player import Player
+
+class ConsolePlayer(Player):
+    
+    def __init__(self, name : str = "") -> None:
+        super().__init__()
+        self.name = name
+
+    def get_move(self, map : Map, first_player : bool) -> int:
+        move = -1
+        possible_moves = map.get_possible_moves(first_player)
+        print(self.name)
+        while move not in possible_moves:
+            move = int(input(f"\nPossible moves: {possible_moves}\nPlease, enter a move: "))
+            if move not in possible_moves:
+                print("Provided move is not possible!!!")
+        return move
