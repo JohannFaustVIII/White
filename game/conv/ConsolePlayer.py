f4d7e1b0f887c67344da68d0584348f6c90ca0d2
Faust VIII <joh.faustviii@gmail.com>
2024-03-06 14:09:54 +0100

    Print map for ConsolePlayer

--- game/conv/ConsolePlayer.py
+++ game/conv/ConsolePlayer.py
@@ -3,12 +3,15 @@ from Player import Player
 
 class ConsolePlayer(Player):
     
-    def __init__(self, name : str = "") -> None:
+    def __init__(self, name : str = "", show_map : bool = False) -> None:
         super().__init__()
         self.name = name
+        self.__show_map = show_map
 
     def get_move(self, map : Map, first_player : bool) -> int:
         move = -1
+        if self.__show_map:
+            self.print_map(map, first_player)
         possible_moves = map.get_possible_moves(first_player)
         print(self.name)
         while move not in possible_moves:
@@ -19,3 +22,32 @@ class ConsolePlayer(Player):
     
     def get_name(self) -> str:
         return "ConsolePlayer"
+    
+    def print_map(self, map, first_player):
+        counter = 0
+        mid_counter = 0
+
+        true_map = [
+            ["\\", "|", "/"],
+            ["-", "0", "-"],
+            ["/", "|", "\\"]
+        ]
+
+        false_map = [
+            [" ", " ", " "],
+            [" ", ".", " "],
+            [" ", " ", " "]
+        ]
+
+        start_map = map.get_points(first_player)
+
+        for p in start_map:
+
+            line = ""
+
+            for v in p:
+                line += true_map[counter][mid_counter] if bool(v) else false_map[counter][mid_counter]
+                mid_counter = (mid_counter + 1) % 3
+
+            counter = (counter + 1) % 3
+            print(line)
